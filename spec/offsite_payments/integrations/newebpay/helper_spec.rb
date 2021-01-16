6f4ad7a77e2c944496c157970f86842daab8c611
蒼時弦也 <elct9620@frost.tw>
2021-01-17 01:07:37 +0800

    Fix timezone not correct config

--- spec/offsite_payments/integrations/newebpay/helper_spec.rb
+++ spec/offsite_payments/integrations/newebpay/helper_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe OffsitePayments::Integrations::Newebpay::Helper do
   let(:config) { Newebpay::Config.new }
 
   before do
-    allow(Time).to receive(:now).and_return(Time.parse('2021-01-01'))
+    allow(Time).to receive(:now).and_return(Time.parse('2021-01-01 00:00:00 +0000'))
     allow(Newebpay::Config).to receive(:current).and_return(config)
 
     Newebpay::Config.config do |c|
@@ -23,7 +23,7 @@ RSpec.describe OffsitePayments::Integrations::Newebpay::Helper do
 
     it { is_expected.to include('MerchantID=DUMMY_MERCHANT') }
     it { is_expected.to include('MerchantOrderNo=DUMMY_ORDER') }
-    it { is_expected.to include('TimeStamp=1609430400') }
+    it { is_expected.to include('TimeStamp=1609459200') }
     it { is_expected.to include('RespondType=JSON') }
   end
 
@@ -35,13 +35,13 @@ RSpec.describe OffsitePayments::Integrations::Newebpay::Helper do
   end
 
   describe '#trade_info' do
-    subject(:trade_info) { helper.trade_info }
+    subject(:trade_info) { pp helper.trade_info }
 
     it do
       expect(trade_info).to eq(
-        'ba096233a931922f34e7273b1e7e81ba7379e4c9ffb1143437349a5a1302b21966f76902fae646bdf98ffd' \
-        '15ee7066e9ffe9a3e3c090fdaef6f59fb059ef2af3af7147a89eec0e11eb5853a55cdc89e87bd07622ff0e' \
-        '048d2b321fd0cc08336a1cdd959f5d72d5d029360c03e2f8467a'
+        'ba096233a931922f34e7273b1e7e81ba7379e4c9ffb1143437349a5a1302b21966f76902fae646bdf98ffd15ee7066e9ffe' \
+        '9a3e3c090fdaef6f59fb059ef2af3af7147a89eec0e11eb5853a55cdc89e8ded40eb9485258c19cdf1da1f2c28c06fa7589' \
+        'f2141922618b17a690cae351af'
       )
     end
   end
@@ -49,6 +49,6 @@ RSpec.describe OffsitePayments::Integrations::Newebpay::Helper do
   describe '#trade_sha' do
     subject { helper.trade_sha }
 
-    it { is_expected.to eq('C60B8AB01C76857E29A977E35115C8AC76EF1328391B59C81FC5F0EDC42C3481') }
+    it { is_expected.to eq('2752103AC2C39B0007D9A27037971E0213ED9028716DB51B408E1F6DA4D74B26') }
   end
 end
